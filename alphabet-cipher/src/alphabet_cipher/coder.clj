b21b314e7384acb4a7a205b5eec90c597aa11ed4
Michael Warmke <mikew670@gmail.com>
2020-05-25 17:02:37 -0500

    implemented alphabet cipher encode

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,25 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:require [clojure.string :as str]))
+
+(def alpha "abcdefghijklmnopqrstuvwxyz")
+
+(defn circularOffset [offset max]
+  (if (>= offset max)
+    (- offset max)
+    offset))
 
 (defn encode [keyword message]
-  "encodeme")
+  (let [keywordChars (take (count message) (cycle (str/split keyword #"")))
+        messageChars (str/split message #"")]
+    (loop [k keywordChars
+           m messageChars
+           buf ""]
+      (if (or (empty? m) (empty? k))
+        buf
+        (let [k-offset (str/index-of alpha (first k))
+              m-offset (str/index-of alpha (first m))
+              offset (circularOffset (+ k-offset m-offset) (count alpha))]
+          (recur (rest k) (rest m) (str buf (get alpha offset))))))))
 
 (defn decode [keyword message]
   "decodeme")
